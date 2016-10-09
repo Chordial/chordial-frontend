45623f75497629ca85049443027e342c9f2e164c
sahilb2 <sahilbhatt@hotmail.com>
2016-10-09 16:21:04 -0500

    onClickSwaghil implemented

--- Chordial/ChordialPage.xaml.cs
+++ Chordial/ChordialPage.xaml.cs
@@ -18,6 +18,10 @@ namespace Chordial
 
 		// Put your OnClick functions for your buttons below here. Use my example!
 
+		async void OnClickSwaghil(object sender, EventArgs e)
+		{
+			await this.DisplayAlert("Swaghil!", "Is Swaghil the best?", "Yes", "Yes \ud83d\ude0e");
+		}
 
 	}
 }
